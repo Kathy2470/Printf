2b22f27fbc75b7666a39b61e092f7e1cfba10fb2
Ayo Happy E <128814468+AyoHappy@users.noreply.github.com>
2023-06-02 19:16:52 +0100

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -3,7 +3,7 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- happy happy ayo mukisa Kathylene
+ * happy ayo mukisa Kathylene
  * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
